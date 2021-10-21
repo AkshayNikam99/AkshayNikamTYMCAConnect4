e611c9a39bb31f6eebabec6ba5e7796346db82e0
AkshayNikam99 <46836590+AkshayNikam99@users.noreply.github.com>
2021-10-22 00:37:42 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter the Rows and Colums : ");
-		ConnectFourGame gameStart=new ConnectFourGame("R","Y",sc.nextInt(),sc.nextInt());
+		System.out.print("Enter the Rows and Colums and ConnectPieceNUmber : ");
+		ConnectFourGame gameStart=new ConnectFourGame("R","Y",sc.nextInt(),sc.nextInt(),sc.nextInt());
         gameStart.startGame();
         
         sc.close();
